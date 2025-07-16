50d2b4acfa4c151aeaac8a4803025e041752ce47
vatsal-vishal <vatsalvishal003@gmail.com>
2025-07-17 02:48:33 +0530

    define a get route for the render url

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const identifyRoute = require('./routes/identify');
 const app = express();
 app.use(express.json());
 
-app.use('/identify', identifyRoute);
+// Add GET / route for instructions
+app.get('/', (req, res) => {
+  res.send('Welcome! To test the POST /identify API, please use Postman or curl.');
+});
 
+app.use('/identify', identifyRoute);
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
